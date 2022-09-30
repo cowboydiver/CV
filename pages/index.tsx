4e53f4b3aef491c38dcdfa9f327613212f0a30ad
CowboyDiver <jandetlefsen@gmail.com>
2022-09-30 13:35:47 +0200

    first animation :)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Container, Flex, Heading } from "@chakra-ui/react";
+import { MotionConfig } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Home: NextPage = () => {
   return (
@@ -19,7 +21,13 @@ const Home: NextPage = () => {
         justifyContent="center"
         background="gray.900"
       >
-        <Heading color="white">Jan Carus</Heading>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 2 }}
+        >
+          <Heading color="white">Jan Carus</Heading>
+        </motion.div>
       </Flex>
     </div>
   );
